2cb149845902bc2aea23af9bb7e6faaeefc4d9d3
sarah-antillia <sarah@antillia.com>
2024-04-08 04:34:54 +0900

    Initial commit

--- projects/TensorflowSlightlyFlexibleUNet/Alzheimer's-Disease/dataset_stat.bat
+++ projects/TensorflowSlightlyFlexibleUNet/Alzheimer's-Disease/dataset_stat.bat
@@ -1,3 +1,3 @@
 rem dataset_stat.bat
 rem 2024/04/08
-python ../../../src/DatasetStatistics.py ../../../dataset/Alzheimers-Disease
+python ../../../src/DatasetStatistics.py ../../../dataset/Alzheimer's-Disease
